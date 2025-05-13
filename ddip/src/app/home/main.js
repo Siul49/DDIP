0841779a6ccc1fe140d0767d0f6f6311adfd80ff
ArgX2 <pkpk0514@gmail.com>
2025-05-13 21:04:28 +0900

    실행 오류 수정 및 grid 함수로 생성

--- ddip/src/app/home/main.js
+++ ddip/src/app/home/main.js
@@ -1,4 +1,6 @@
 export function Main() {
+
+
     return (
         <div className={"relative w-full h-full bg-[#FFFCED] flex justify-center text-center"}>
             <div className="relative top-0 w-full h-[80vh] bg-[url(/textured-paper.png)] flex justify-center">
@@ -12,18 +14,13 @@ export function Main() {
                 </div>
 
                 <div className={"absolute bg-gray-20 w-[75%] h-8 bottom-8 rounded-2xl"}>
-                    <div
-                        className={"absolute w-full h-full grid grid-cols-10 gap-x-15 items-center justify-center"}>
-                        <button className={"min-w-16 w-full h-10 bg-gray-500 hover:bg-gray-400 rounded-full select-transition"}></button>
-                        <button className={"min-w-16 w-full h-10 bg-gray-500 hover:bg-gray-400 rounded-full select-transition"}></button>
-                        <button className={"min-w-16 w-full h-10 bg-gray-500 hover:bg-gray-400 rounded-full select-transition"}></button>
-                        <button className={"min-w-16 w-full h-10 bg-gray-500 hover:bg-gray-400 rounded-full select-transition"}></button>
-                        <button className={"min-w-16 w-full h-10 bg-gray-500 hover:bg-gray-400 rounded-full select-transition"}></button>
-                        <button className={"min-w-16 w-full h-10 bg-gray-500 hover:bg-gray-400 rounded-full select-transition"}></button>
-                        <button className={"min-w-16 w-full h-10 bg-gray-500 hover:bg-gray-400 rounded-full select-transition"}></button>
-                        <button className={"min-w-16 w-full h-10 bg-gray-500 hover:bg-gray-400 rounded-full select-transition"}></button>
-                        <button className={"min-w-16 w-full h-10 bg-gray-500 hover:bg-gray-400 rounded-full select-transition"}></button>
-                        <button className={"min-w-16 w-full h-10 bg-gray-500 hover:bg-gray-400 rounded-full select-transition"}></button>
+                    <div className={"absolute w-full h-full grid grid-cols-10 gap-x-15 items-center justify-center"}>
+                        {Array.from({ length: 10 }).map((_, index) => (
+                            <button
+                                key={index}
+                                className="min-w-16 w-full h-10 bg-gray-500 hover:bg-gray-400 rounded-full select-transition"
+                            ></button>
+                        ))}
                     </div>
                 </div>
 
@@ -34,22 +31,18 @@ export function Main() {
 
             <h2 className={"absolute top-220 w-full font-bold text-3xl"}>카테고리</h2>
             <div className={"absolute top-240 bg-gray-200 w-[75%] h-65 grid grid-cols-6 rounded-2xl"}>
-                <img src="/testimage.png" className=" w-full h-full p-4 object-cover hover:bg-gray-300 select-transition" alt="이미지"/>
-                <img src="/testimage.png" className=" w-full h-full p-4 object-cover hover:bg-gray-300 select-transition" alt="이미지"/>
-                <img src="/testimage.png" className=" w-full h-full p-4 object-cover hover:bg-gray-300 select-transition" alt="이미지"/>
-                <img src="/testimage.png" className=" w-full h-full p-4 object-cover hover:bg-gray-300 select-transition" alt="이미지"/>
-                <img src="/testimage.png" className=" w-full h-full p-4 object-cover hover:bg-gray-300 select-transition" alt="이미지"/>
-                <img src="/testimage.png" className=" w-full h-full p-4 object-cover hover:bg-gray-300 select-transition" alt="이미지"/>
-            </div>
+                {Array.from({ length: 6 }).map((_, index) => (
+                    <img src="/testimage.png"
+                         className=" w-full h-full p-4 object-cover hover:bg-gray-300 select-transition" alt="이미지"/>
+                ))}
+         </div>
 
             <h2 className={"absolute top-320 w-full font-bold text-3xl"}>마감 직전 상품 목록</h2>
             <div className={"absolute top-340 bg-gray-200 w-[75%] h-65 grid grid-cols-6 rounded-2xl"}>
-                <img src="/testimage.png" className=" w-full h-full p-4 object-cover hover:bg-gray-300 select-transition" alt="이미지"/>
-                <img src="/testimage.png" className=" w-full h-full p-4 object-cover hover:bg-gray-300 select-transition" alt="이미지"/>
-                <img src="/testimage.png" className=" w-full h-full p-4 object-cover hover:bg-gray-300 select-transition" alt="이미지"/>
-                <img src="/testimage.png" className=" w-full h-full p-4 object-cover hover:bg-gray-300 select-transition" alt="이미지"/>
-                <img src="/testimage.png" className=" w-full h-full p-4 object-cover hover:bg-gray-300 select-transition" alt="이미지"/>
-                <img src="/testimage.png" className=" w-full h-full p-4 object-cover hover:bg-gray-300 select-transition" alt="이미지"/>
+                {Array.from({ length: 6 }).map((_, index) => (
+                    <img src="/testimage.png"
+                         className=" w-full h-full p-4 object-cover hover:bg-gray-300 select-transition" alt="이미지"/>
+                ))}
             </div>
         </div>
     )
