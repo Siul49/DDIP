59289d50cf14e99aca76d4e5357cf6a3049b2fea
ArgX2 <pkpk0514@gmail.com>
2025-05-16 16:30:48 +0900

    카테고리 이미지 추가, constants/simpleDb에 카테고리 key 및 value 리스트 저장, key값 받아 value.png이미지 출력

--- ddip/src/app/home/category.js
+++ ddip/src/app/home/category.js
@@ -1,6 +1,6 @@
 import Image from 'next/image'
 import { useState } from 'react';
-
+import category_lists from '@constants/simpleDb';
 export function Category({onSelect}) {
 
     const [activeIndex, setActiveIndex] = useState(null);
@@ -12,9 +12,9 @@ export function Category({onSelect}) {
         <section className="relative w-full flex flex-col items-center justify-center bg-[#FFFCED]">
             <h2 className={"absolute w-full top-16 font-bold text-3xl text-center"}>카테고리</h2>
             <div className={"relative top-30 bg-gray-200 w-[75%] h-65 grid grid-cols-6 rounded-2xl"}>
-                {Array.from({ length: 6 }).map((_,index_category) => (
-                    <button key={index_category} onClick={() => handleClick(index_category+1)} className="relative w-full h-full">
-                        <Image src={'/testimage.png'} fill
+                {category_lists.map((item,index) => (
+                    <button key={item.key} onClick={() => handleClick(Number(index.key)+1)} className="relative w-full h-full">
+                        <Image src={`/categoryImg/${item.value}.png`} fill
                                className=" w-full h-full p-4 object-contain hover:bg-gray-300 select-transition" alt="이미지"/>
                     </button>
                 ))}
---
+++ ddip/src/constants/simpleDb.js
@@ -0,0 +1,11 @@
+const category_lists=
+    [
+        {key:'0', value:'ingredient'},
+        {key:'1', value:'instant'},
+        {key:'2', value:'stuffs'},
+        {key:'3', value:'large'},
+        {key:'4', value:'deliver'},
+        {key:'5', value:'donate'},
+    ];
+
+export default category_lists;
