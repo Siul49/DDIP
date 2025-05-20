ecf7acb010153afc9a218b55d9ac6ee5e23ec3a7
ArgX2 <pkpk0514@gmail.com>
2025-05-20 21:26:30 +0900

    상품페이지 제작중

--- ddip/src/app/page.js
+++ ddip/src/app/page.js
@@ -56,7 +56,7 @@ export default function Home() {
 
         {mode === 'product' && (
             <>
-                <Item onSelect={setSelectedProduct} />
+                <Item onSelect={selectedProduct} />
             </>
         )}
 
