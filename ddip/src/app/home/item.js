ff7494dce7a0c14b423570d914b2e355adcc55f3
ArgX2 <pkpk0514@gmail.com>
2025-05-22 11:30:35 +0900

    상품페이지 다듬기

--- ddip/src/app/home/item.js
+++ ddip/src/app/home/item.js
@@ -26,16 +26,20 @@ export function Item({selectedProduct, onSelect }) {
                 <h3 className={"absolute w-[20%] h-[10%] top-120 left-10 bg-green-300 font-bold text-2xl"}>
                     현재인원
                 </h3>
-                <div className={`absolute w-[25%] bottom-10 left-0 gap-4 grid grid-cols-${productData.team}`}>
+                <div className={`absolute w-[25%] bottom-10 left-0 gap-4 grid grid-cols-4`}>
                     {Array.from({ length: productData.now }).map((_, idx) => (
                         <div key={idx} className="relative aspect-square rounded-full bg-[#000000]">
                                 <Image src="/testimage.png" fill
                                        className=" w-full h-full p-2 object-fit rounded-full" alt="이미지"/>
                             </div>
                         ))}
+                    {Array.from({ length: productData.team-productData.now }).map((_, idx) => (
+                        <div key={idx} className="relative aspect-square rounded-full bg-[#000000]">
+                            <div className=" w-full h-full p-2 bg-white border-4 rounded-full"></div>
+                        </div>
+                    ))}
                 </div>
             </div>
-
         </section>
     )
 }
