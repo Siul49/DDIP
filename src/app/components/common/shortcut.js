614e92c78f35f3ee4befcf287b8ab034b6374404
ArgX2 <pkpk0514@gmail.com>
2025-06-06 14:27:51 +0900

    오류 해결

--- src/app/components/common/shortcut.js
+++ src/app/components/common/shortcut.js
@@ -1,7 +1,6 @@
 import Image from 'next/image';
 import ChatList from '../chat/chat-list';
-import Chatting from '../chat/chatting';
-import ChatBox from './chatting';
+import ChatBox from '../chat/chatting';
 import PostBox from './post';
 
 import {useEffect, useState} from "react";
@@ -22,8 +21,7 @@ export default function Shortcut({}) {
                 onClick={toggleChat}
             ><Image src="/chat.png" fill alt="chat" className="object-contain p-2"/>
             </button>
-            {isOpen && <ChatList onClose={toggleChat} />}
-            {isCOpen && <ChatBox onClose={toggleChat} />}
+                {isCOpen && <ChatBox onClose={toggleChat} />}
 
             <button
                 className="fixed bottom-20 right-5 p-6 bg-[#FADD88] text-white rounded-full transition z-50
