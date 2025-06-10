113c3f5da1379eebaf8167d3498834dbd47e7708
ArgX2 <pkpk0514@gmail.com>
2025-06-10 23:38:40 +0900

    상품 등록 모달 이미지 첨부 텍스트 수정

--- src/app/components/post/post-box.js
+++ src/app/components/post/post-box.js
@@ -134,14 +134,22 @@ export default function PostBox({ onClose }) {
                     {/* 이미지 업로드 */}
                     <div className="flex items-center">
                         <label className="w-26 text-md font-semibold text-[#4D4D4D]">상품이미지</label>
-                        <input
-                            type="file"
-                            className="w-full border border-[#E0E0E0] rounded-lg p-2"
-                            name="image"
-                            accept="image/*"
-                            onChange={handleImageChange}
-                        />
-                        {form.image && <span className="ml-2">{form.image.name}</span>}
+                        <div className={"w-full border border-[#E0E0E0] rounded-lg p-2"}>
+                            <a className="absolute">
+                                {form.image ? (
+                                    <span className="ml-2">{form.image.name}</span>
+                                ) : (
+                                    "여기에 파일을 넣으세요."
+                                )}
+                            </a>
+                            <input
+                                type="file"
+                                className="w-full  opacity-0"
+                                name="image"
+                                accept="image/*"
+                                onChange={handleImageChange}
+                            />
+                        </div>
                     </div>
 
                     {/* 제목 */}
